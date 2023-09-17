af408e39ecad40173d21fb5a2f891f0bd0561aea
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-09-16 21:50:12 -0400

    fixed home path

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/', (req, res) => res.send('Hello World!'));
+router.get('/', (req, res) => res.send('Hello World!'));
 // router.get('/ryan', (req, res) => res.send('Hello Ryan!'));
 
 router.use('/contacts', require('./contacts'));
