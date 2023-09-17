a54c148e008aa5ccaab829e04cfa2ddb7b3752fb
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-09-16 21:12:54 -0400

    commented out old connection file

--- db/connection.js
+++ db/connection.js
@@ -1,28 +1,28 @@
-const { MongoClient } = require("mongodb");
-const uri = process.env.MONGODB_URI;
+// const { MongoClient } = require("mongodb");
+// const uri = process.env.MONGODB_URI;
 
-async function main() {
+// async function main() {
   
-  const client = new MongoClient(uri);
+//   const client = new MongoClient(uri);
 
-  try {
-    // Connect to the MongoDB cluster
-    await client.connect();
+//   try {
+//     // Connect to the MongoDB cluster
+//     await client.connect();
 
-    // Make the appropriate DB calls
-    await listDatabases(client);
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
+//     // Make the appropriate DB calls
+//     await listDatabases(client);
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
 
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
+// async function listDatabases(client) {
+//   databasesList = await client.db().admin().listDatabases();
 
-  console.log("Databases:");
-  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
-}
+//   console.log("Databases:");
+//   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
